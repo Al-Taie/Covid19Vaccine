dd15229b29e735a8660eddafefecf973c3d2a528
Al-Taie <agprosup@gmail.com>
2021-08-31 18:34:28 +0300

    fix: remove unnecessary code

--- app/src/main/java/com/melon/covid_19_vaccine/ui/fragments/StatisticsFragment.kt
+++ app/src/main/java/com/melon/covid_19_vaccine/ui/fragments/StatisticsFragment.kt
@@ -1,6 +1,5 @@
 package com.melon.covid_19_vaccine.ui.fragments
 
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
@@ -8,8 +7,6 @@ import com.melon.covid_19_vaccine.data.DataManager
 import com.melon.covid_19_vaccine.databinding.FragmentStatisticsBinding
 import com.melon.covid_19_vaccine.ui.BaseFragment
 import com.melon.covid_19_vaccine.ui.StatisticsAdapter
-import com.melon.covid_19_vaccine.util.initPieChart
-import com.melon.covid_19_vaccine.util.initTopFiveBinding
 
 
 /**
@@ -24,15 +21,10 @@ class StatisticsFragment : BaseFragment<FragmentStatisticsBinding>() {
 
     override fun setup() {
         DataManager.initTop10()
-        initTopFiveBinding(binding = binding)
-        initPieChart(binding = binding)
+//        initTopFiveBinding(binding = binding)
+//        initPieChart(binding = binding)
 
-        // Log.v("DataManager=====",DataManager.getVaccinatedTop5.toString())
-        //  val adapter = StatisticsAdapter(DataManager.getVaccineListCountry())
-
-
-        val adapter = StatisticsAdapter(DataManager.getVaccinatedTop5)
-        binding.recyclerCountry.adapter = adapter
+        binding.recyclerCountry.adapter = StatisticsAdapter(DataManager.getVaccinatedTop10)
 
     }
 
