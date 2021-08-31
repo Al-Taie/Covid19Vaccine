aa298352bd33103c8e054a07da684e07a21626e5
Al-Taie <agprosup@gmail.com>
2021-08-31 18:28:00 +0300

    update: statistics adapter

--- app/src/main/java/com/melon/covid_19_vaccine/ui/StatisticsAdapter.kt
+++ app/src/main/java/com/melon/covid_19_vaccine/ui/StatisticsAdapter.kt
@@ -1,31 +1,34 @@
 package com.melon.covid_19_vaccine.ui
 
-import android.graphics.Color
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.melon.covid_19_vaccine.R
 import com.melon.covid_19_vaccine.data.domain.Vaccinated
-import com.melon.covid_19_vaccine.ui.viewHolders.StatisticsViewHolder
+import com.melon.covid_19_vaccine.databinding.ItemStatisticsBinding
+import com.melon.covid_19_vaccine.util.getVaccinated
 
-class StatisticsAdapter(val list:List<List<Vaccinated>>) : RecyclerView.Adapter<StatisticsViewHolder>() {
+class StatisticsAdapter(private val list: List<List<Vaccinated>>) :
+    RecyclerView.Adapter<StatisticsAdapter.StatisticsViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StatisticsViewHolder {
-        val view =
-            LayoutInflater.from(parent.context).inflate(R.layout.item_statistics, parent, false)
+        val item = R.layout.item_statistics
+        val view = LayoutInflater.from(parent.context).inflate(item, parent, false)
         return StatisticsViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: StatisticsViewHolder, position: Int) {
-        val currentCountry = list[position]
-
-        holder.apply {
-            countryNameOne.text = currentCountry.get(0).country
-            totalCase.text=currentCountry.get(5).totalVaccinations.toString()
+        with(list.getVaccinated(position = position)) {
+            holder.binding.apply {
+                countryName.text = country
+                totalCases.text = totalVaccinations.toString()
+            }
         }
-
-
     }
 
     override fun getItemCount() = list.size
 
+    class StatisticsViewHolder(viewItem: View) : RecyclerView.ViewHolder(viewItem) {
+        val binding = ItemStatisticsBinding.bind(viewItem)
+    }
 }
--- app/src/main/java/com/melon/covid_19_vaccine/util/statisticsFunctions.kt
+++ app/src/main/java/com/melon/covid_19_vaccine/util/statisticsFunctions.kt
@@ -7,85 +7,65 @@ import com.razerdp.widget.animatedpieview.AnimatedPieViewConfig
 import com.razerdp.widget.animatedpieview.data.SimplePieInfo
 
 
-fun initPieChart(binding: FragmentStatisticsBinding) {
-    val config = AnimatedPieViewConfig()
-    with(DataManager.getVaccinatedTop10) {
-        config.apply {
-            startAngle(-40f)
-            duration(1800)
-            drawText(true)
-            canTouch(true)
-//            strokeMode(true)
-//            strokeWidth(8)
-            textSize(36F)
-            textMargin(3)
-            splitAngle(1.5F)
-            interpolator(DecelerateInterpolator())
-            autoSize(false)
-            pieRadiusRatio(1F)
-            animOnTouch(true)
-            focusAlphaType(AnimatedPieViewConfig.FOCUS_WITH_ALPHA_REV)
-            focusAlpha(100)
-            floatShadowRadius(4F)
-            floatExpandSize(4F)
-
-            addData(
-                SimplePieInfo(
-                    firstCountry().totalVaccinations.toDouble(),
-                   randomColor, firstCountry().country
-                )
-            )
-
-            addData(
-                SimplePieInfo(
-                    secondCountry().totalVaccinations.toDouble(),
-                    randomColor, secondCountry().country
-                )
-            )
-
-            addData(
-                SimplePieInfo(
-                    thirdCountry().totalVaccinations.toDouble(),
-                    randomColor, thirdCountry().country
-                )
-            )
-
-            addData(
-                SimplePieInfo(
-                    fourthCountry().totalVaccinations.toDouble(),
-                    randomColor, fourthCountry().country
-                )
-            )
-
-            addData(
-                SimplePieInfo(
-                    lastCountry().totalVaccinations.toDouble(),
-                    randomColor, lastCountry().country
-                )
-            )
-
-        }
-    }
-    binding.pieChart.applyConfig(config)
-    binding.pieChart.start()
-}
-
-
-fun initTopFiveBinding(binding: FragmentStatisticsBinding) {
-    DataManager.vaccineList.sortByDescending { it.last().totalVaccinations }
-    with(DataManager.getVaccinatedTop10) {
-        binding.apply {
-            countryNameOne.text = firstCountry().country
-            countryNameTwo.text = secondCountry().country
-            countryNameThird.text = thirdCountry().country
-            countryNameFourth.text = fourthCountry().country
-            countryNameFifth.text = lastCountry().country
-
-            totalCasesOne.text = firstCountry().totalVaccinations.toString()
-            totalCasesTwo.text = secondCountry().totalVaccinations.toString()
-            totalCasesThird.text = thirdCountry().totalVaccinations.toString()
-            totalCasesFourth.text = fourthCountry().totalVaccinations.toString()
-            totalCasesFifth.text = lastCountry().totalVaccinations.toString()
-        }
-    }
-}
+//fun initPieChart(binding: FragmentStatisticsBinding) {
+//    val config = AnimatedPieViewConfig()
+//    with(DataManager.getVaccinatedTop10) {
+//        config.apply {
+//            startAngle(-40f)
+//            duration(1800)
+//            drawText(true)
+//            canTouch(true)
+////            strokeMode(true)
+////            strokeWidth(8)
+//            textSize(36F)
+//            textMargin(3)
+//            splitAngle(1.5F)
+//            interpolator(DecelerateInterpolator())
+//            autoSize(false)
+//            pieRadiusRatio(1F)
+//            animOnTouch(true)
+//            focusAlphaType(AnimatedPieViewConfig.FOCUS_WITH_ALPHA_REV)
+//            focusAlpha(100)
+//            floatShadowRadius(4F)
+//            floatExpandSize(4F)
+//
+//            addData(
+//                SimplePieInfo(
+//                    firstCountry().totalVaccinations.toDouble(),
+//                   randomColor, firstCountry().country
+//                )
+//            )
+//
+//            addData(
+//                SimplePieInfo(
+//                    secondCountry().totalVaccinations.toDouble(),
+//                    randomColor, secondCountry().country
+//                )
+//            )
+//
+//            addData(
+//                SimplePieInfo(
+//                    thirdCountry().totalVaccinations.toDouble(),
+//                    randomColor, thirdCountry().country
+//                )
+//            )
+//
+//            addData(
+//                SimplePieInfo(
+//                    fourthCountry().totalVaccinations.toDouble(),
+//                    randomColor, fourthCountry().country
+//                )
+//            )
+//
+//            addData(
+//                SimplePieInfo(
+//                    lastCountry().totalVaccinations.toDouble(),
+//                    randomColor, lastCountry().country
+//                )
+//            )
+//
+//        }
+//    }
+//    binding.pieChart.applyConfig(config)
+//    binding.pieChart.start()
+//}
