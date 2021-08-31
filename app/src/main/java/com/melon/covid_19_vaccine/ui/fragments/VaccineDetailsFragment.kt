3d2ff1d54163dd5e33eaa81eeb740ecdd8691ed8
Al-Taie <agprosup@gmail.com>
2021-08-31 19:30:46 +0300

    fix: vaccine details class

--- app/src/main/java/com/melon/covid_19_vaccine/ui/fragments/VaccineDetailsFragment.kt
+++ app/src/main/java/com/melon/covid_19_vaccine/ui/fragments/VaccineDetailsFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.melon.covid_19_vaccine.R
 import com.melon.covid_19_vaccine.data.domain.Details
 import com.melon.covid_19_vaccine.databinding.FragmentVaccineDetailsBinding
 import com.melon.covid_19_vaccine.ui.BaseFragment
@@ -18,6 +17,11 @@ import com.melon.covid_19_vaccine.util.Constant
 class VaccineDetailsFragment : BaseFragment<FragmentVaccineDetailsBinding>() {
     override fun setup() {}
 
+    override fun callBack() {}
+
+    override val inflate: (LayoutInflater, ViewGroup?, attachToRoot: Boolean) -> FragmentVaccineDetailsBinding
+        get() = FragmentVaccineDetailsBinding::inflate
+
     override fun onStart() {
         super.onStart()
         arguments?.let {
@@ -26,32 +30,28 @@ class VaccineDetailsFragment : BaseFragment<FragmentVaccineDetailsBinding>() {
         }
     }
 
-    private fun bindData(data: Details) {
+    private fun bindData(details: Details) {
         binding.apply {
-            name.text = data.data[Constant.VaccineDetailsIndex.VACCINE_NAME].toString()
-            dosesPercent.text = data.data[Constant.VaccineDetailsIndex.DOSE_TAKEN].toString()
-            efficacyPercent.text = data.data[Constant.VaccineDetailsIndex.EFFECTIVENESS].toString()
-            nameText.text = data.data[Constant.VaccineDetailsIndex.NAME].toString()
-            manufacturer.text = data.data[Constant.VaccineDetailsIndex.MANUFACTURER].toString()
-            typeText.text = data.data[Constant.VaccineDetailsIndex.TYPE_OF_VACCINE].toString()
-            numberText.text = data.data[Constant.VaccineDetailsIndex.NUMBER_OF_SHOT].toString()
-            howGivenText.text = data.data[Constant.VaccineDetailsIndex.HOW_GIVEN].toString()
-            vaccineImage.setImageResource(data.data[Constant.VaccineDetailsIndex.IMAGE_SRC].toString().toInt())
+            with(details) {
+                name.text = data[Constant.VaccineDetailsIndex.VACCINE_NAME]
+                dosesPercent.text = data[Constant.VaccineDetailsIndex.DOSE_TAKEN]
+                efficacyPercent.text = data[Constant.VaccineDetailsIndex.EFFECTIVENESS]
+                nameText.text = data[Constant.VaccineDetailsIndex.NAME]
+                manufacturer.text = data[Constant.VaccineDetailsIndex.MANUFACTURER]
+                typeText.text = data[Constant.VaccineDetailsIndex.TYPE_OF_VACCINE]
+                numberText.text = data[Constant.VaccineDetailsIndex.NUMBER_OF_SHOT]
+                howGivenText.text = data[Constant.VaccineDetailsIndex.HOW_GIVEN]
+                vaccineImage.setImageResource(data[Constant.VaccineDetailsIndex.IMAGE_SRC].toInt())
+            }
         }
-
     }
-
-    override fun callBack() {}
-
-    override val inflate: (LayoutInflater, ViewGroup?, attachToRoot: Boolean) -> FragmentVaccineDetailsBinding
-        get() = FragmentVaccineDetailsBinding::inflate
 
     companion object {
 
         fun newInstance(data: Details) = VaccineDetailsFragment().apply {
-                arguments = Bundle().apply {
-                    putSerializable(Constant.VaccineName.VACCINE_DETAILS, data)
-                }
+            arguments = Bundle().apply {
+                putSerializable(Constant.VaccineName.VACCINE_DETAILS, data)
+            }
         }
     }
 }
